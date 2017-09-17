c87dd825d90a10901782acae63d13695a117be83
sergdalm <30310397+sergdalm@users.noreply.github.com>
2017-09-17 23:35:13 +0300

    Сделаны исправления, минимализация

--- js/script.js
+++ js/script.js
@@ -10,44 +10,44 @@ var storageemail = localStorage.getItem("email");
 
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  popup.classList.add("modal-show");
-  if (storagename) {
-    username.value = storagename;
-  }
-  if (storagename && storageemail){
-    username.value = storagename;
-    email.value = storageemail;
-  }
-  else {
-    username.focus();
-  }
+    popup.classList.add("modal-show");
+    if (storagename) {
+        username.value = storagename;
+    }
+    if (storagename && storageemail) {
+        username.value = storagename;
+        email.value = storageemail;
+    }
+    else {
+        username.focus();
+    }
 });
 
 close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
-});
-
-form.addEventListener("submit", function(evt){
-  if (!name.value || !email.value || !text.value) {
     evt.preventDefault();
-    popup.classList.add("modal-error");
-    console.log("Нужно ввести имя, e-mail и текст письма");
-  }
-  else {
-    localStorage.setItem ("name", name.value);
-    localStorage.setitem ("email", email.value);
-    console.log("Форма отправлена");
-  }
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
 });
 
-window.addEventListener("keydown", function(evt) {
-  if (popup.classList.contains("modal-show")){
-    if (evt.keyCode == 27) {
-    popup.classList.remove("modal-show");
+form.addEventListener("submit", function (evt) {
+    if (!name.value || !email.value || !text.value) {
+        evt.preventDefault();
+        popup.classList.add("modal-error");
+        console.log("Нужно ввести имя, e-mail и текст письма");
     }
-  }
+    else {
+        localStorage.setItem("name", name.value);
+        localStorage.setitem("email", email.value);
+        console.log("Форма отправлена");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (popup.classList.contains("modal-show")) {
+        if (evt.keyCode == 27) {
+            popup.classList.remove("modal-show");
+        }
+    }
 });
